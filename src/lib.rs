396ccd0b8970c5ab469d8be54749d17497cece13
newt <hi@newty.dev>
2025-07-02 15:55:28 +0100

    feat: initial commit

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+// todo: endpoints
+// - projects
+// - versions
+// - version-files
+// - users
+// - notifications
+// - threads
+// - teams
+// - tags
+// - misc
+
+// todo: utils
+// - mrpack downloader
+// - curseforge -> modrinth
